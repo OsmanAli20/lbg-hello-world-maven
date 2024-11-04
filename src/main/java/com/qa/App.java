501d405afe263a27c19803e3187328fb8c3e9d7c
unknown <ozmanmail14@gmail.com>
2024-11-04 14:33:12 +0000

    new name added

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Osman Ali"));
+        System.out.println(sayHelloToSomeone("Ali"));
         System.out.println(sayGoodbye());
     }
 
